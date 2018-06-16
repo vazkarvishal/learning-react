18115e80808b33eba5ca2b9871e315eb0d9872b8
MMTDIGITAL\vishal.v <vishal.v@mmtdigital.co.uk>
2018-06-16 23:42:32 +0100

    cleanup counter code file

--- indecision-app/src/playground/counter-example.js
+++ indecision-app/src/playground/counter-example.js
@@ -16,7 +16,7 @@ class Counter extends React.Component {
         }
       })
     }
-    
+
     handleMinusOne() {
       this.setState((prevState) => {
         return {
@@ -45,43 +45,3 @@ class Counter extends React.Component {
 }
 
 ReactDOM.render(<Counter />, document.getElementById('app'))
-
-// ================================================
-// Above code is written using React component state
-// ================================================
-
-
-// let count = 0
-// const addOne = () => {
-//   count++
-//   renderCounterApp()
-// }
-
-// const minusOne = () => {
-//   count--
-//   renderCounterApp()
-// }
-
-// const reset = () => {
-//   count = 0
-//   renderCounterApp()
-// }
-
-
-// const appRoot = document.getElementById('app');
-
-// const renderCounterApp = () => {
-
-//   const templateTwo = (
-//     <div>
-//       <h1>Count: {count}</h1>
-//       <button onClick={addOne}>+1</button>
-//       <button onClick={reset}>reset</button>
-//       <button onClick={minusOne}>-1</button>
-//     </div>
-//   );
-
-//   ReactDOM.render(templateTwo, appRoot)
-// }
-
-// renderCounterApp()
